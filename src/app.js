c19e6d280bc9f10621224cabd4717960917230fd
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-04 01:31:59 -0600

    feat(api): update todos API version to v1

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/todos', todo);
+app.use('/api/v1/todos', todo);
 
 app.use((req, res, next) => res.status(404).json({ message: 'Not found!' }));
 app.use((err, req, res, next) => res.status(500).json({ message: err.message }));
